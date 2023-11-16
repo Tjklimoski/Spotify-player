f43ef21f6b93ae2a29e48544e42574e1e3b6aa4b
TJ Klimoski <tjklimoski@gmail.com>
2023-11-16 14:27:53 -0500

    Prevent page refreshing when user hits enter on search bar

--- client/src/Searchbar.jsx
+++ client/src/Searchbar.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from "react";
 
 export default function Searchbar({ search, handleOnChange }) {
   return (
-    <form className="search-form">
+    <form className="search-form" onSubmit={e => e.preventDefault()}>
       <input
         className="search"
         type="search"
         placeholder="Search Song/Artists"
         value={search}
-        onChange={(e) => handleOnChange(e.target.value)}
+        onChange={e => handleOnChange(e.target.value)}
       />
     </form>
   );

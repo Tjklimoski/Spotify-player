a5da9525db60358cb80cab121f4b7d522c186f64
TJ Klimoski <tjklimoski@gmail.com>
2023-11-16 14:39:29 -0500

    Add keyboard accesibility to moving through and selecting search results

--- client/src/TrackSearchResult.jsx
+++ client/src/TrackSearchResult.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
+import React from "react";
 
 export default function TrackSearchResult({ track, chooseTrack }) {
-
   function handlePlay() {
     chooseTrack(track);
   }
 
+  function handleKeyDown(e) {
+    if (e.key === "Enter") handlePlay();
+  }
+
   return (
-    <div className="search-result" onClick={handlePlay}>
-      <img src={track.albumUrl} alt="album img"/>
+    <div
+      className="search-result"
+      onClick={handlePlay}
+      tabIndex={0}
+      aria-label={`Play ${track.title}`}
+      onKeyDown={handleKeyDown}
+    >
+      <img src={track.albumUrl} alt="album img" />
       <div className="track-info">
         <div className="track-title">{track.title}</div>
         <div className="track-artists">
           {track.artists.map(artist => {
-            return <span key={artist.name}>{artist.name}</span>
+            return <span key={artist.name}>{artist.name}</span>;
           })}
         </div>
       </div>

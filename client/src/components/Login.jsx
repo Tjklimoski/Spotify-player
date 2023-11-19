149781b3a0c13bda2b5e7d10f1f96acd68d39374
TJ Klimoski <tjklimoski@gmail.com>
2023-11-19 15:26:49 -0500

    Create vercel.json file to handle vite SPA redirects

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import qs from 'qs';
+import React from "react";
+import qs from "qs";
 
-const SPOTIFY_ENDPOINT = "https://accounts.spotify.com/authorize?"
+const SPOTIFY_ENDPOINT = "https://accounts.spotify.com/authorize?";
 
 const SPOTIFY_QUERY = {
   client_id: "c1632634c2b2457cb93a760c789aaef4",
   response_type: "code",
-  redirect_uri: "http://localhost:5173/",
+  redirect_uri: `${window.location.origin}/`,
   scope:
     "streaming user-read-email user-read-private user-library-read user-library-modify user-read-playback-state user-modify-playback-state",
 };
@@ -14,5 +14,9 @@ const SPOTIFY_QUERY = {
 const AUTH_URL = SPOTIFY_ENDPOINT + qs.stringify(SPOTIFY_QUERY);
 
 export default function Login() {
-  return <a className="btn" href={AUTH_URL}>Login with Spotify</a>
+  return (
+    <a className="btn" href={AUTH_URL}>
+      Login with Spotify
+    </a>
+  );
 }
